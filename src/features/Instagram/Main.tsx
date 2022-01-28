b4cabdd704ec4d4ad5424caffe13400b0fddd201
sajad <sajad@sajads-MacBook-Pro.local>
2022-01-28 15:20:57 +0330

    first commit + ui

---
+++ src/features/Instagram/Main.tsx
@@ -0,0 +1,64 @@
+import { Avatar, Button, Grid, Paper, Typography } from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShareRoundedIcon from '@mui/icons-material/ShareRounded';
+import ChatBubbleOutlineRoundedIcon from '@mui/icons-material/ChatBubbleOutlineRounded';
+import BookmarkBorderRoundedIcon from '@mui/icons-material/BookmarkBorderRounded';
+import instapost from '../../instagram-image.jpeg'
+const Main = () => {
+  return (
+    <>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Paper
+          elevation={3}
+          sx={{ width: "20%", height: "380px", borderRadius: "20px" }}
+        >
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="center"
+            flexDirection="column"
+            height="100%"
+          >
+            <Grid
+              container
+              alignItems="center"
+              justifyContent="space-between"
+              padding={2}
+            >
+              <Grid item>
+                <Grid container alignItems="center" flexDirection="row">
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                  <Typography sx={{ padding: "0 7px" }}>user_name</Typography>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined" size="small">
+                  Follow
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container justifyContent='center' alignItems='center'>
+              <img
+                width='98%'
+                src={instapost}
+                alt="post"
+              />
+            </Grid>
+            <Grid container justifyContent='space-between' alignItems='center' padding={3}>
+                <Grid item ><FavoriteIcon color="error"/><ChatBubbleOutlineRoundedIcon sx={{margin:"0 5px"}}/><ShareRoundedIcon /></Grid>
+                <Grid item><BookmarkBorderRoundedIcon /></Grid>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/features/Instagram/MainSlice.ts
@@ -0,0 +1,53 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState, AppThunk } from '../../app/store';
+// import { fetchCount } from './counterAPI';
+
+export interface CounterState {
+  value: number;
+  status: 'idle' | 'loading' | 'failed';
+}
+
+const initialState: CounterState = {
+  value: 0,
+  status: 'idle',
+};
+
+// export const incrementAsync = createAsyncThunk(
+//   'counter/fetchCount',
+//   async (amount: number) => {
+//     const response = await fetchCount(amount);
+//     // The value we return becomes the `fulfilled` action payload
+//     return response.data;
+//   }
+// );
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {},
+   // extraReducers: (builder) => {
+  //   builder
+  //     .addCase(incrementAsync.pending, (state) => {
+  //       state.status = 'loading';
+  //     })
+  //     .addCase(incrementAsync.fulfilled, (state, action) => {
+  //       state.status = 'idle';
+  //       state.value += action.payload;
+  //     });
+  // },
+});
+
+export const { } = counterSlice.actions;
+export const selectCount = (state: RootState) => state.counter.value;
+
+// export const incrementIfOdd = (amount: number): AppThunk => (
+//   dispatch,
+//   getState
+// ) => {
+//   const currentValue = selectCount(getState());
+//   if (currentValue % 2 === 1) {
+//     dispatch(incrementByAmount(amount));
+//   }
+// };
+
+export default counterSlice.reducer;
